16ac426f025f8955b70666ba1e6285106343437c
Vedangi1018 <76875162+Vedangi1018@users.noreply.github.com>
2023-05-06 12:18:56 +0530

    Update HelloJ.java

--- HelloJ.java
+++ HelloJ.java
@@ -1,6 +1,8 @@
-
-class HelloJ{
-    public static void main(String[] args) {
+class HelloJ
+{
+    public static void main(String[] args) 
+    {
         System.out.println("Hello Jenkins"); 
+        System.out.println("Jenkins Java");
     }
 }
